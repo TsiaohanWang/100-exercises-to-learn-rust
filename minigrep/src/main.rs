cf1246e4b2341c6f1ce5db6ec8faf83eac291fe6
Wxh1104 <wxh8953717@icloud.com>
2025-10-07 22:35:19 +0800

    增加笔记内容

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,11 +1,28 @@
+use std::sync::mpsc;
 use std::thread;
+use std::time::Duration;
 
 fn main() {
-    let v = vec![1, 2, 3];
+    let (tx, rx) = mpsc::channel();
 
-    let handle = thread::spawn(|| {
-        println!("Here's a vector: {v:?}");
+    let send = thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            println!("Send: {val}");
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs_f64(0.5));
+        }
     });
-
-    handle.join().unwrap();
+send.join().unwrap();
+    loop {
+        let receive = rx.recv().unwrap();
+    println!("Receive: {receive}");
+    }
+    
 }
